caaa4354d0eed095e907ed98ba42be41bb554659
Tony Tan <tonytan0@icloud.com>
2022-04-20 15:16:53 -0400

    finished the Recipe model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from recipes.models import Recipe
 
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " by " + self.author
