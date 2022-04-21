d8bce8a37767830f414867c7db1454516b096e1e
Tony Tan <tonytan0@icloud.com>
2022-04-21 18:48:08 -0400

    I'm sure this would work

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os import dj_database_url
+import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
