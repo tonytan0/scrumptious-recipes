1e084e34a2ff9466b8b4b4e5c596d3f5f593c516
Tony Tan <tonytan0@icloud.com>
2022-04-21 15:05:02 -0400

    added markdown formatting to description

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
 ]
 
 MIDDLEWARE = [
