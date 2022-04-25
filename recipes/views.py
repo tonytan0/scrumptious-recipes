3cd7d93a014294dbe5e4e42dfb1862a95bdf25be
Tony Tan <tonytan0@icloud.com>
2022-04-25 17:42:02 -0400

    Added the update view for recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
 
@@ -13,40 +13,6 @@ try:
 except Exception:
     RecipeForm = None
     Recipe = None
-
-
-# def create_recipe(request):
-#     if request.method == "POST" and RecipeForm:
-#         form = RecipeForm(request.POST)
-#         if form.is_valid():
-#             recipe = form.save()
-#             return redirect("recipe_detail", pk=recipe.pk)
-#     elif RecipeForm:
-#         form = RecipeForm()
-#     else:
-#         form = None
-#     context = {
-#         "form": form,
-#     }
-#     return render(request, "recipes/new.html", context)
-
-
-def change_recipe(request, pk):
-    if Recipe and RecipeForm:
-        instance = Recipe.objects.get(pk=pk)
-        if request.method == "POST":
-            form = RecipeForm(request.POST, instance=instance)
-            if form.is_valid():
-                form.save()
-                return redirect("recipe_detail", pk=pk)
-        else:
-            form = RecipeForm(instance=instance)
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/edit.html", context)
 
 
 class RecipeListView(ListView):
@@ -64,21 +30,6 @@ class RecipeDetailView(DetailView):
         return context
 
 
-# def show_recipes(request):
-#     context = {
-#         "recipes": Recipe.objects.all() if Recipe else [],
-#     }
-#     return render(request, "recipes/list.html", context)
-
-
-# def show_recipe(request, pk):
-#     context = {
-#         "recipe": Recipe.objects.get(pk=pk) if Recipe else None,
-#         "rating_form": RatingForm(),  # highlight
-#     }
-#     return render(request, "recipes/detail.html", context)
-
-
 def log_rating(request, recipe_id):
     if request.method == "POST":
         form = RatingForm(request.POST)
@@ -93,4 +44,11 @@ class RecipeCreateView(CreateView):
     model = Recipe
     template_name = "recipes/new.html"
     fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
+    fields = ["title", "author", "description", "image"]
     success_url = reverse_lazy("recipes_list")
