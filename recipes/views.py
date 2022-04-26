d8cd3c28be82208df7da3db6960db5c60a38cdc3
Tony Tan <tonytan0@icloud.com>
2022-04-26 18:33:30 -0400

    Refactored the templates to harden the app

--- recipes/views.py
+++ recipes/views.py
@@ -14,13 +14,12 @@ def log_rating(request, recipe_id):
     if request.method == "POST":
         form = RatingForm(request.POST)
         if form.is_valid():
-            rating = form.save(commit=False)
             try:
+                rating = form.save(commit=False)
                 rating.recipe = Recipe.objects.get(pk=recipe_id)
                 rating.save()
             except Recipe.DoesNotExist:
                 return redirect("recipes_list")
-    # return redirect("recipe_detail", pk=recipe_id)
 
 
 class RecipeListView(ListView):
