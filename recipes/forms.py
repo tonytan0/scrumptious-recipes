88edcc63c5f1406feab5948580b3b0395fee1621
Tony Tan <tonytan0@icloud.com>
2022-04-26 20:02:29 -0400

    added authors to recipes on create and edit

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 
-try:
-    from recipes.models import Recipe
+# try:
+#     from recipes.models import Recipe
 
-    class RecipeForm(forms.ModelForm):
-        class Meta:
-            model = Recipe
-            fields = [
-                "name",
-                "author",
-                "description",
-                "image",
-            ]
-
-except Exception:
-    pass
-
+# class RecipeForm(forms.ModelForm):
+#     class Meta:
+#         model = Recipe
+#         fields = [
+#             "name",
+#             "author",
+#             "description",
+#             "image",
+#         ]
 
 from recipes.models import Rating
 
